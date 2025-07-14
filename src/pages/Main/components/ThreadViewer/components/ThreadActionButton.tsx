d2557260e320da21ac5b9667b1bcf5aea4d33d5f
Joao Eira <simbyotic@gmail.com>
2025-07-14 14:40:01 +0200

    task: move thread

--- src/pages/Main/components/ThreadViewer/components/ThreadActionButton.tsx
+++ src/pages/Main/components/ThreadViewer/components/ThreadActionButton.tsx
@@ -1,6 +1,6 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Array as Arr, Effect, pipe } from "effect";
-import { Ellipsis, Trash } from "lucide-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Array as Arr, Effect, Option, pipe } from "effect";
+import { ArrowLeft, ArrowRight, Ellipsis, Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -12,6 +12,37 @@ import type { Session } from "@/domain/session/schema";
 import { SessionService } from "@/domain/session/service";
 import type { Thread } from "@/domain/thread/schema";
 import { ThreadService } from "@/domain/thread/service";
+
+async function getThreadPosition(
+  sessionId: Session["id"],
+  threadId: Thread["id"]
+): Promise<"first" | "last" | "middle"> {
+  const program = Effect.gen(function* () {
+    const sessionService = yield* SessionService;
+    const threads = yield* sessionService.getThreads(sessionId);
+
+    const threadIndex = Arr.findFirstIndex(
+      threads,
+      (thread) => thread.id === threadId
+    ).pipe(Option.getOrThrow);
+
+    const position =
+      threadIndex === 0
+        ? "first"
+        : threadIndex === threads.length - 1
+        ? "last"
+        : "middle";
+
+    return position;
+  });
+
+  return Effect.runPromise(
+    program.pipe(
+      Effect.provide(SessionService.Default),
+      Effect.provide(ThreadService.Default)
+    )
+  );
+}
 
 function deleteThreadEffect(threadId: Thread["id"], sessionId: Session["id"]) {
   const program = Effect.gen(function* () {
@@ -36,6 +67,43 @@ function deleteThreadEffect(threadId: Thread["id"], sessionId: Session["id"]) {
   );
 }
 
+function moveThreadEffect(
+  threadId: Thread["id"],
+  sessionId: Session["id"],
+  direction: "left" | "right"
+) {
+  const program = Effect.gen(function* () {
+    const sessionService = yield* SessionService;
+
+    const threadIds = yield* sessionService
+      .getThreads(sessionId)
+      .pipe(Effect.map((threads) => threads.map((thread) => thread.id)));
+
+    const threadIndex = Arr.findFirstIndex(
+      threadIds,
+      (id) => id === threadId
+    ).pipe(Option.getOrThrow);
+
+    const newThreadIds = pipe(
+      Arr.remove(threadIds, threadIndex),
+      Arr.insertAt(
+        direction === "left" ? threadIndex - 1 : threadIndex + 1,
+        threadId
+      ),
+      Option.getOrThrow
+    );
+
+    yield* sessionService.update(sessionId, {
+      threads: newThreadIds,
+    });
+  });
+
+  return program.pipe(
+    Effect.provide(SessionService.Default),
+    Effect.provide(ThreadService.Default)
+  );
+}
+
 type ThreadActionButtonProps = {
   sessionId: Session["id"];
   threadId: Thread["id"];
@@ -47,11 +115,31 @@ export default function ThreadActionButton({
 }: ThreadActionButtonProps) {
   const queryClient = useQueryClient();
 
+  const { data: threadPosition } = useQuery({
+    queryKey: ["thread-position", sessionId, threadId],
+    queryFn: () => getThreadPosition(sessionId, threadId),
+  });
+
   const { mutate: deleteThread } = useMutation({
     mutationFn: () =>
       Effect.runPromise(deleteThreadEffect(threadId, sessionId)),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["threads", sessionId] });
+    },
+  });
+
+  const { mutate: moveThread } = useMutation({
+    mutationFn: (direction: "left" | "right") =>
+      Effect.runPromise(moveThreadEffect(threadId, sessionId, direction)),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["threads", sessionId] });
+      queryClient.invalidateQueries({
+        queryKey: ["thread-position"],
+      });
+      document.querySelector(`[data-thread-id="${threadId}"]`)?.scrollIntoView({
+        behavior: "instant",
+        block: "nearest",
+      });
     },
   });
 
@@ -63,6 +151,20 @@ export default function ThreadActionButton({
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
+        <DropdownMenuItem
+          onClick={() => moveThread("left")}
+          disabled={threadPosition && threadPosition === "first"}
+        >
+          <ArrowLeft className="w-3 h-3" />
+          Move Left
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => moveThread("right")}
+          disabled={threadPosition && threadPosition === "last"}
+        >
+          <ArrowRight className="w-3 h-3" />
+          Move Right
+        </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => deleteThread()}
           className="text-destructive"
